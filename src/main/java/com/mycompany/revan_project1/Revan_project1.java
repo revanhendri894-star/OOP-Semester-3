55ef9f0c9ce9bb3a20f3a3de1c52c739a0e0a762
revanhendri894-star <revanhendri894@gmail.com>
2026-09-01 10:52:12 +0700

    PBO laprak 1

---
+++ src/main/java/com/mycompany/revan_project1/Revan_project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.revan_project1;
+
+/**
+ *
+ * @author WORKPLUS
+ */
+public class Revan_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama  : Emmanuel Revan Hendri Wijaya");
+        System.out.println("NPM   : 2505060009");
+        System.out.println("Alamat: Bantul, Daerah Istimewa Yogyakarta");
+        System.out.println("No_HP : 088250506009");
+    }
+}
